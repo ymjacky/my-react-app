a43ec1f690e909ed5bbd2f008d0818f23da5215e
ymjacky <jacky.service@icloud.com>
2019-06-30 13:26:34 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import useReactRouter from 'use-react-router';
+
 import logo from './logo.svg';
 import './App.css';
+
+function Hello() {
+  const { history, location, match } = useReactRouter();
+  return (
+    <div>
+      <h1>HelloWorld</h1>
+      <p>{`pathname: ${location.pathname}`}</p>
+      <button onClick={() => history.push('/react')}>Next</button>
+    </div>
+  );
+}
+
+function HelloReact() {
+  const { history, location, match } = useReactRouter();
+  return (
+    <div>
+      <h1>HelloReact</h1>
+      <p>{`pathname: ${location.pathname}`}</p>
+      <button onClick={() => history.push('/')}>Next</button>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
@@ -8,11 +33,16 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Excampe</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <Router>
+            <Switch>
+              <Route path="/" component={Hello} exact />
+              <Route path="/react" component={HelloReact} exact />
+            </Switch>
+          </Router>
+        </div>
       </div>
     );
   }
